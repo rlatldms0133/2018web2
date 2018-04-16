4d3b31529ce1b4a23094b7705e499563eab59875
rlatldms0133 <37520439+rlatldms0133@users.noreply.github.com>
2018-04-16 11:23:28 +0900

    sdhs

---
+++ sdhs0413/js/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+    var c=0;
+    var inter;
+    function slider(){
+        inter = setInterval(function(){
+            if(c>=3000) {
+                $("#main_slider > #img_box").animate({left:"0"});
+                c = 0;
+            }
+            else{
+                $("#main_slider > #img_box").animate({left:"-=1000"});
+                c+=1000;
+            }
+        },2500)
+    }
+
+    slider();
+
+});
